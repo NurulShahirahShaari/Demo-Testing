ed7331b257186a65a794d39dad8db05a2c41ee9f
shahirahhh <shahirahshaari@gmail.com>
2021-02-17 08:22:20 +0800

    go

---
+++ print.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt"
+		"strconv"
+	)
+
+func main()  {
+	
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[0]) //in byte
+	fmt.Println("Hello World"[3:7])
+	fmt.Println("Hello World"[3:]) //tak pedulikan yg akhir
+
+	//conversion from string to int
+
+	var x string = "100"
+
+	xInt, _ := strconv.Atoi(x)
+	fmt.Println(xInt)
+
+	xString := strconv.Itoa(-100)
+	fmt.Println(xString)
+
+}
